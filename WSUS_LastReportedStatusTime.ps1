588dbcfd28f8b7f39b4af6c2ce4e1b7ddd16c173
ruleito <52785935+ruleito@users.noreply.github.com>
2023-07-07 15:38:27 +0300

    Update WSUS_LastReportedStatusTime.ps1

--- WSUS_LastReportedStatusTime.ps1
+++ WSUS_LastReportedStatusTime.ps1
@@ -13,52 +13,54 @@ $encoding = [System.Text.Encoding]::UTF8
 $failed_hosts = @()
 
 foreach ($comp in $computers) {
-    if ($comp.OSDescription -like "*Server*") {
+    if ($comp.OSdescription -like "*Server*") {
         $hash_table_servers[$comp.FullDomainName] = @{
             LastReportedStatusTime = $comp.LastReportedStatusTime
             IPAddress = $comp.IPAddress
-            OSDescription = $comp.OSDescription
+            OSdescription = $comp.OSdescription
         }
     }
-    elseif ($comp.OSDescription -notmatch "Server") {
+    elseif ($comp.OSdescription -notmatch "Server") {
         $hash_table_wks[$comp.FullDomainName] = @{
             LastReportedStatusTime = $comp.LastReportedStatusTime
             IPAddress = $comp.IPAddress
-            OSDescription = $comp.OSDescription
+            OSdescription = $comp.OSdescription
         }
     }
 }
 
 function Test-Array ($arrayName) {
-    $failed_hosts = @()
+$failed_hosts = @()
 
-    if (-not $arrayName.GetType().Name.Equals("Hashtable")) { return }
-    foreach ($key in $arrayName.Keys) {
-        try {
-            $pingResult = Test-Connection -ComputerName $key -Count 1 -Quiet
-            if ($pingResult) {
-                $scriptBlock = {
-                    if ([Environment]::OSVersion.Version.Major -ge 10) {
-                        usoclient.exe StartScan
-                    } else {
-                        wuauclt.exe /resetauthorization /detectnow /reportnow
-                    }
+if (-not $arrayName.GetType().Name.Equals("Hashtable")) { return }
+foreach ($key in $arrayName.Keys) {
+    try {
+        $pingResult = Test-Connection -ComputerName $key -Count 1 -Quiet
+        if ($pingResult) {
+            $scriptBlock = {
+                if ([Environment]::OSVersion.Version.Major -ge 10) {
+                    usoclient.exe StartScan
+                } else {
+                    wuauclt.exe /resetauthorization
+                    wuauclt /detectnow /reportnow
                 }
-                Invoke-Command -ComputerName $key -ScriptBlock $scriptBlock | Out-File $logFile -Append
-                Write-Host "Command executed successfully on host: $key"
             }
-            else {
-                Write-Host "$key is not reachable"
-                $failed_hosts += $key
-            }
+            # Invoke-Command -ComputerName $key -ScriptBlock $scriptBlock | Out-File $logFile -Append
+            Invoke-Command -ComputerName $key -ScriptBlock {Get-Service} | Out-File $logFile -Append
+            Write-Host "Command executed successfully on host: $key"
         }
-        catch {
-            Write-Host "Error executing command on host $key : $($PSItem.Exception.Message)" | Out-File $logFile -Append
+        else {
+            Write-Host "$key is not reachable"
             $failed_hosts += $key
         }
     }
+    catch {
+        Write-Host "Error executing command on host $key : $($PSItem.Exception.Message)"
+        $failed_hosts += $key
+    }
+}
 
-    return $failed_hosts
+return $failed_hosts
 }
 
 if ($hash_table_servers.Count -gt 0) {
@@ -68,13 +70,24 @@ if ($hash_table_servers.Count -gt 0) {
 
 Write-Host $failed_hosts.Count
 if ($failed_hosts.Count -gt 0) {
-    $Body = "The following hosts have failed to execute the command. LastReportedStatusTime on these hosts is 72h+: <br><br>"
+    $Body = "The following hosts have failed to execute the command. LastReportedStatusTime on this hosts 72h+ : <br><br>"
     foreach ($hosts in $failed_hosts) {
         $Body += $hosts + "<br>"
     }
     try {
         Write-Host "Email body: $Body"
-        Send-MailMessage -From $From -to $To -Subject $Subject -BodyAsHtml -Body $Body -SmtpServer $SMTPServer -Port $SMTPPort  -Encoding $encoding -ErrorAction Stop -Verbose
+        Send-MailMessage -From $From -to $To -Subject $Subject -Bodyashtml -Body $Body -SmtpServer $SMTPServer -Port $SMTPPort  -Encoding $encoding -ErrorAction Stop -Verbose
+        Write-Host $Body
+    }
+    catch {
+        Write-Host "Failed to send email: $($PSItem.Exception.Message)"
+    }
+}
+else {
+    $body = "When executing the script, no errors were found."
+     try {
+        Write-Host "Email body: $Body"
+        Send-MailMessage -From $From -to $To -Subject $Subject -Bodyashtml -Body $Body -SmtpServer $SMTPServer -Port $SMTPPort  -Encoding $encoding -ErrorAction Stop -Verbose
     }
     catch {
         Write-Host "Failed to send email: $($PSItem.Exception.Message)"
@@ -88,16 +101,27 @@ if ($hash_table_wks.Count -gt 0) {
 }
 
 if ($failed_hosts.Count -gt 0) {
-    $Body = "The following hosts have failed to execute the command. LastReportedStatusTime on these hosts is 72h+ !!!helpdesk!!!: <br><br>"
+    $Body = "The following hosts have failed to execute the command. LastReportedStatusTime on this hosts 72h+ !!!helpdesk!!!: <br><br>"
     foreach ($hosts in $failed_hosts) {
         $Body += $hosts + "<br>"
     }
     try {
         Write-Host "Email body: $Body"
-        Send-MailMessage -From $From -to $To -Subject $Subject -BodyAsHtml -Body $Body -SmtpServer $SMTPServer -Port $SMTPPort  -Encoding$encoding -ErrorAction Stop -Verbose
+        Send-MailMessage -From $From -to $To -Subject $Subject -Bodyashtml -Body $Body -SmtpServer $SMTPServer -Port $SMTPPort  -Encoding $encoding -ErrorAction Stop -Verbose
+        Write-Host $Body
     }
     catch {
         Write-Host "Failed to send email: $($PSItem.Exception.Message)"
     }
 }
-(Get-WsusServer).GetSubscription().StartSynchronization()
+
+else {
+    $body = "When executing the script, no errors were found."
+     try {
+        Write-Host "Email body: $Body"
+        Send-MailMessage -From $From -to $To -Subject $Subject -Bodyashtml -Body $Body -SmtpServer $SMTPServer -Port $SMTPPort  -Encoding $encoding -ErrorAction Stop -Verbose
+    }
+    catch {
+        Write-Host "Failed to send email: $($PSItem.Exception.Message)"
+    }
+}
